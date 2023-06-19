b206eee8f77264da2776ee5e132243a50ea6ccb9
AmareshBale <137062501+AmareshBale@users.noreply.github.com>
2023-06-19 19:50:42 +0530

    Update Compressor.java

--- Compress & Decompress(Project)/comp_decmp/Compressor.java
+++ Compress & Decompress(Project)/comp_decmp/Compressor.java
@@ -9,11 +9,13 @@ import java.util.zip.GZIPOutputStream;
 public class Compressor {
     public static void method(File file) throws IOException {
         String fileDirectory = file.getParent();
-        FileInputStream fis = new FileInputStream(file);
+        FileInputStream fis = new FileInputStream(file);  //Input for the compression
         FileOutputStream fos = new FileOutputStream(fileDirectory+"/CompressedFile.gz");
-        GZIPOutputStream gzip = new GZIPOutputStream(fos);
+        GZIPOutputStream gzip = new GZIPOutputStream(fos);   //Output
         byte[] buffer = new byte[1024];
         int len;
+        
+        //Reading and Compressing the File
         while ((len=fis.read(buffer))!=-1) {
             gzip.write(buffer, 0, len);
         }
